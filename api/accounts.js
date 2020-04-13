1ced0b82c475928438799cf646c96af329b64c4c
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-13 11:04:40 -0600

    Add GET `/api/accounts/:id` route

--- api/accounts.js
+++ api/accounts.js
@@ -9,6 +9,28 @@ router.get("/", (req, res) => {
     .from("accounts")
     .then((accounts) => {
       res.status(200).json(accounts);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Could not retrieve accounts data" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db.select()
+    .from("accounts")
+    .where("id", req.params.id)
+    .first()
+    .then((account) => {
+      if (account) {
+        res.status(200).json(account);
+      } else {
+        res
+          .status(404)
+          .json({ error: "Account with the specified id could not be found" });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Could not retrieve accounts data" });
     });
 });
 
